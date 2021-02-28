948fccc3f045bcd48927101019c702130385dd9c
Ruslan <rqelenderli7@gmail.com>
2021-02-28 17:59:52 +0300

    All Template

---
+++ All/Task3d/Task3d/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3d.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Feature()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Prcing()
+        {
+            return View();
+        }
+        
+    }
+}
